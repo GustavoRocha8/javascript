6b519e52692ead7a4ec1a2f99262b933717a03f3
Gustavo Rocha <142359575+GustavoRocha8@users.noreply.github.com>
2023-10-14 11:35:28 -0300

    Resolução de Exercicio

---
+++ aula16ex/script.js
@@ -0,0 +1,19 @@
+let num = document.getElementById('nmtxt')
+let tab = document.getElementById('selnum')
+let res = document.getElementById('res')
+let valores = []
+
+
+function adicionar() {
+
+    if (isNumero(num.value) && ! inTab(num.value)) {
+
+    }
+    } 
+
+}
+
+function finalizar() {
+
+}
+
